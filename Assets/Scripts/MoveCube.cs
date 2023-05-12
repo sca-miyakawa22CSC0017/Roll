6fb194d2f06be816f5f6b3d2f5ac2cd417e5c23f
宮川 昂大 <SDT22CSC0017@edu.sca.ac.jp>
2023-05-12 17:47:14 +0900

    アップデート

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : Trap
+{
+    [SerializeField]
+    float moveX;
+    [SerializeField]
+    float moveY;
+    [SerializeField]
+    float moveZ;
+    [SerializeField]
+    float speed;
+
+    float step;
+    bool goBack = false;
+    Vector3 origin;
+    Vector3 destination;
+
+
+    protected override void Start()
+    {
+        origin = transform.position;
+        destination = new Vector3(origin.x - moveX, origin.y - moveY, origin.z - moveZ);
+    }
+
+protected override void Update()
+    {
+        if (stop)
+        {
+            return;
+        }
+        step = speed * Time.deltaTime;
+
+        if (!goBack)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,destination, step);
+
+            if(transform.position == destination)
+            {
+                goBack = true;
+
+                StartCoroutine(Wait());
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, origin,step);
+
+            if(transform.position == origin)
+            {
+                goBack = false;
+
+                StartCoroutine(Wait());
+            }
+        }
+    }
+}
